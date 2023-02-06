7c9180b89e0a77cefc1bb6b83e1b592db30eaaec
stztek <sairatzmedia@gmail.com>
2023-02-05 22:16:35 -0300

    sbcms

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
     <ContextDisc>
       <Context>
         <div className="App">
-          <BrowserRouter basename="/post">
+          <BrowserRouter>
             <Suspense >
               <Nav />
             </Suspense>
